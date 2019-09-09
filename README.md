4b4c389afd05b18847f1862dda32ad67af94057a
Apple <dalejan@bigappcomapny.in>
2019-09-09 23:24:16 +0530

    create app.js for create server using some default port 2

---
+++ README.md
@@ -0,0 +1,2 @@
+//1. create app.js for create server and install espress for middleware.
+// 2. server run at 1230 port by default.
--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 let express = require("express");
-let bodyParser = require("body-parser");
 var http = require("http");
 
 let app = express();
-app.use(bodyParser.json());
 
-app.use("/", require(__dirname + "/routes"));
 var port = 1230;
 var server = http.createServer(app);
 server.listen(port);
